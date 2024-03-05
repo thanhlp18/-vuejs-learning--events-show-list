bf57366320df42774f57d826c5fc85a705959b15
Phuoc Thanh <thanhlp18@gmail.com>
2024-03-05 15:07:25 +0700

    deploy to render.com

---
+++ dist/assets/About-C-T4TXRg.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o,a as s}from"./index-BZOAeJMo.js";const c={},n={class:"about"},r=s("h1",null,"A site for events to better the world.",-1),_=[r];function a(d,i){return o(),t("div",n,_)}const f=e(c,[["render",a]]);export{f as default};
